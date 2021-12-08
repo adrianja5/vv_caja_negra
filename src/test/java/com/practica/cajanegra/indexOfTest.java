59ba28ef9d1593e24b248ef3e72b8092c0c87891
adrianja5 <adrian.jimenez.aguilar@alumnos.upm.es>
2021-12-08 19:31:17 +0100

    Añadidos tests de indexOf e isEmpty

---
+++ src/test/java/com/practica/cajanegra/indexOfTest.java
@@ -0,0 +1,45 @@
+package com.practica.cajanegra;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import com.cajanegra.SingleLinkedListImpl;
+
+class indexOfTest {
+	
+	private static SingleLinkedListImpl<Character> lista;
+	
+	@BeforeAll
+	static void inicializarLista() {
+		lista = new SingleLinkedListImpl<>('A', 'B', 'M', 'D', 'T', 'D', 'Y', 'Z');
+	}
+
+	@ParameterizedTest(name="[{index}] -> elem=''{0}'', salidaEsperada={1}")
+	@CsvSource({
+		"'A', 1",
+		"'B', 2",
+		"'D', 4",
+		"'Y', 7",
+		"'Z', 8",
+	})
+	void indexOfValidoTest(Character elem, int salidaEsperada) {
+		assertEquals(salidaEsperada, lista.indexOf(elem));
+	}
+	
+	@ParameterizedTest(name="[{index}] -> elem=''{0}''")
+	@CsvSource({
+		"'X'",
+		"'@'",
+		"'['",
+	})
+	void indexOfInvalidoTest(Character elem) {
+		assertThrows(NoSuchElementException.class, () -> {
+			lista.indexOf(elem);
+		});
+	}
+
+}
---
+++ src/test/java/com/practica/cajanegra/isEmptyTest.java
@@ -0,0 +1,31 @@
+package com.practica.cajanegra;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.cajanegra.SingleLinkedListImpl;
+
+class isEmptyTest {
+	
+	private static SingleLinkedListImpl<Character> listaLlena;
+	private static SingleLinkedListImpl<Character> listaVacia;
+	
+	@BeforeAll
+	static void inicializarLista() {
+		listaLlena = new SingleLinkedListImpl<>('A', 'B', 'C', 'D');
+		listaVacia = new SingleLinkedListImpl<>();
+	}
+
+	@Test
+	void isEmptyLlenoTest() {
+		assertEquals(false, listaLlena.isEmpty());
+	}
+	
+	@Test
+	void isEmptyVacioTest() {
+		assertEquals(true, listaVacia.isEmpty());
+	}
+
+}
